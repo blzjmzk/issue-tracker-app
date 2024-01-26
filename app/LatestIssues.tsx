a3dd457bf420aa5c3300b68c17f020fe49a76564
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-01-26 16:21:57 +0100

    Troubleshooting

--- app/LatestIssues.tsx
+++ app/LatestIssues.tsx
@@ -1,6 +1,7 @@
 import { Avatar, Card, Flex, Heading, Table } from "@radix-ui/themes";
 import { IssueStatusBadge } from "./components";
 import Link from "next/link";
+import prisma from "@/prisma/client";
 
 const LatestIssues = async () => {
   const issues = await prisma?.issue.findMany({
